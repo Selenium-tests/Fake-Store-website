a07f0d819d0ae26e5d6cdceb1926ab413d1a60ae
aksi1984 <pablo_xmen@op.pl>
2023-09-15 14:17:26 +0200

    Added clickElement() method which uses the JavascriptExecutor

--- src/main/java/qa/base/BasePage.java
+++ src/main/java/qa/base/BasePage.java
@@ -1,16 +1,27 @@
 package qa.base;
 
+import io.cucumber.java.et.Ja;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
 
     WebDriver driver;
+    JavascriptExecutor javascriptExecutor;
 
     public BasePage(WebDriver driver) {
 
         this.driver = driver;
+
+        javascriptExecutor = (JavascriptExecutor) driver;
         PageFactory.initElements(this.driver, this);
+    }
+
+    public void clickElement(WebElement element) {
+
+        javascriptExecutor.executeScript("arguments[0].click()", element);
     }
 
     public String getTitle() {
