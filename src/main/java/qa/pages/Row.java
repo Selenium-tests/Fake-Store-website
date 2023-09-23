3f6737012c54ea11bc6c3b3ebb4c47eb40d778ad
aksi1984 <pablo_xmen@op.pl>
2023-09-23 11:20:47 +0200

    Added locators and methods

--- src/main/java/qa/pages/Row.java
+++ src/main/java/qa/pages/Row.java
@@ -9,7 +9,10 @@ public class Row extends BasePage {
 
     private WebElement removeButton;
     private WebElement productName;
+    private WebElement amountField;
     private WebElement price;
+    private WebElement totalPrice;
+
     public Row(WebDriver driver) {
 
         super(driver);
@@ -19,12 +22,14 @@ public class Row extends BasePage {
 
         removeButton = product.findElement(By.xpath(".//a[@class='remove']"));
         productName = product.findElement(By.className("product-name"));
+        amountField = product.findElement(By.xpath(".//input[@type='number']"));
         price = product.findElement(By.xpath(".//span[@class='woocommerce-Price-amount amount']"));
+        totalPrice = product.findElement(By.xpath(".//td[@class='product-subtotal']"));
     }
 
     public void clickRemoveButton() {
 
-        removeButton.click();
+        clickElement(removeButton);
     }
 
     public String getName() {
@@ -35,5 +40,16 @@ public class Row extends BasePage {
     public String getPrice() {
 
         return price.getText();
+    }
+
+    public void setAmount(String amount) {
+
+        amountField.clear();
+        amountField.sendKeys(amount);
+    }
+
+    public String getTotalPrice() {
+
+        return totalPrice.getText();
     }
 }
