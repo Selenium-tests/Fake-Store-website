aa7e3dea9df33ce9cabd4d11b5396c52e017c1ba
aksi1984 <pablo_xmen@op.pl>
2023-09-30 22:51:09 +0200

    Created ShoppingCartSummary class

---
+++ src/main/java/qa/pages/ShoppingCartSummary.java
@@ -0,0 +1,43 @@
+package qa.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import qa.base.BasePage;
+
+public class ShoppingCartSummary extends BasePage {
+
+    public ShoppingCartSummary(WebDriver driver) {
+
+        super(driver);
+    }
+
+    @FindBy(xpath = ".//td[@data-title='Kwota']")
+    WebElement amount;
+
+    @FindBy(xpath = ".//tr[@class='cart-discount coupon']")
+    WebElement couponDescription;
+
+    @FindBy(xpath = ".//td[@data-title='Suma']")
+    WebElement sum;
+
+    public String getAmount() {
+
+        return amount.getText();
+    }
+
+    public String getCouponDescription(String couponCode) {
+
+        String xpath = ".//tr[@class='cart-discount coupon-" + couponCode + "']";
+
+        WebElement element = getDriver().findElement(By.xpath(xpath));
+
+        return element.findElement(By.xpath(".//th")).getText();
+    }
+
+    public String getSum() {
+
+        return sum.getText();
+    }
+}
