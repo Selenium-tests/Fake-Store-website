f09ffa209ec6460b4301745901aaf8c043e05ea4
aksi1984 <pablo_xmen@op.pl>
2023-10-06 23:04:02 +0200

    Created the LinksToEditAddressesRunner class

---
+++ src/test/java/steps/billingAddressForm/LinksToEditAddressesRunner.java
@@ -0,0 +1,14 @@
+package steps.billingAddressForm;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "./src/main/resources/linksToEditAddresses.feature",
+        plugin = {"pretty", "html:target/cucumber-reports/adding-to-cart.html"}
+)
+public class LinksToEditAddressesRunner {
+}
