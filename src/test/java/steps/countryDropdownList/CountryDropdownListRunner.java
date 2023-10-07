7ef3e7373714a10ab8b0c8a49772a0ea6277850a
aksi1984 <pablo_xmen@op.pl>
2023-10-07 23:40:04 +0200

    Created the CountryDropdownListRunner class

---
+++ src/test/java/steps/countryDropdownList/CountryDropdownListRunner.java
@@ -0,0 +1,14 @@
+package steps.countryDropdownList;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "./src/main/resources/countryDropdownList.feature",
+        plugin = {"pretty", "html:target/cucumber-reports/adding-to-cart.html"}
+)
+public class CountryDropdownListRunner {
+}
