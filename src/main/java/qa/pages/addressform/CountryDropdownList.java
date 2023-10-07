24cdcbb850bccf8af15a29bb0415512f1e76906c
aksi1984 <pablo_xmen@op.pl>
2023-10-07 23:39:27 +0200

    Added Actions to click the drop-down list arrow

--- src/main/java/qa/pages/addressform/CountryDropdownList.java
+++ src/main/java/qa/pages/addressform/CountryDropdownList.java
@@ -1,25 +1,35 @@
 package qa.pages.addressform;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import qa.base.BasePage;
+import java.util.List;
 
 
 public class CountryDropdownList extends BasePage {
+
     public CountryDropdownList(WebDriver driver) {
 
         super(driver);
     }
 
-    @FindBy(css = ".select2-selection__arrow")
-    WebElement button;
+    @FindBy(xpath = ".//span[@class='select2-selection__arrow']")
+    WebElement arrow;
 
     @FindBy(xpath = "//input[@class='select2-search__field']")
     WebElement searchField;
 
-    public void clickButton() {
+    @FindBy(xpath = ".//li[@class='select2-results__option select2-results__message']")
+    List<WebElement> message;
 
-        clickElement(button);
+    public void clickArrow() {
+
+        WebElement element = getWait().until(ExpectedConditions.elementToBeClickable(arrow));
+
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(element).click().perform();
     }
 
     public void setCountry(String country) {
@@ -27,5 +37,8 @@ public class CountryDropdownList extends BasePage {
         searchField.sendKeys(country + Keys.ENTER);
     }
 
+    public boolean isMessageDisplayed() {
 
+        return message.isEmpty();
+    }
 }
