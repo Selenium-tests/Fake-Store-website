139a63241d98cab093bbf5b24b621068e1f988c5
aksi1984 <pablo_xmen@op.pl>
2023-10-07 23:37:22 +0200

    Added the WebDriverWait instance

--- src/main/java/qa/base/BasePage.java
+++ src/main/java/qa/base/BasePage.java
@@ -1,25 +1,39 @@
 package qa.base;
 
-import io.cucumber.java.et.Ja;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BasePage {
 
-    WebDriver driver;
-    JavascriptExecutor javascriptExecutor;
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+    private final JavascriptExecutor javascriptExecutor;
 
     public BasePage(WebDriver driver) {
 
         this.driver = driver;
 
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         javascriptExecutor = (JavascriptExecutor) driver;
+
         PageFactory.initElements(this.driver, this);
     }
 
-    public void clickElement(WebElement element) {
+    private @FindBy(xpath = ".//a[@class='woocommerce-store-notice__dismiss-link']")
+    WebElement notice;
+
+    public void hideNotice() {
+
+        notice.click();
+    }
+
+    protected void clickElement(WebElement element) {
 
         javascriptExecutor.executeScript("arguments[0].click()", element);
     }
@@ -34,8 +48,13 @@ public class BasePage {
         return driver.getCurrentUrl();
     }
 
-    public WebDriver getDriver() {
+    protected WebDriver getDriver() {
 
         return driver;
+    }
+
+    protected WebDriverWait getWait() {
+
+        return wait;
     }
 }
