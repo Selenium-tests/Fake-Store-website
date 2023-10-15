49f4823f46ac6db876ae059daff581195a185a9d
aksi1984 <pablo_xmen@op.pl>
2023-10-15 17:52:40 +0200

    Changed the class name

---
+++ src/test/java/com/stepdefs/login/Runner.java
@@ -0,0 +1,15 @@
+package com.stepdefs.login;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "./src/main/resources/features/login/login.feature",
+        plugin = { "pretty", "html:target/cucumber-reports/login.html" },
+        glue = {"com.stepdefs.login", "com.commonstepdefs"}
+)
+public class Runner {
+}
