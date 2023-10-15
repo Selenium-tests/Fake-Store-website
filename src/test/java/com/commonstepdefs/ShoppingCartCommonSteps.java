bf8324525d182249b52ad2cf6c83f3ec04eae446
aksi1984 <pablo_xmen@op.pl>
2023-10-15 17:56:37 +0200

    Created the ShoppingCartCommonSteps class

---
+++ src/test/java/com/commonstepdefs/ShoppingCartCommonSteps.java
@@ -0,0 +1,26 @@
+package com.commonstepdefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import qa.pages.ProductsContainer;
+import qa.pages.SiteHeaderCart;
+
+import static qa.driver.Driver.*;
+public class ShoppingCartCommonSteps {
+
+    @Given("The product number {int} from the {string} category has been added to the shopping cart")
+    public void theProductHasBeenAddedToTheShoppingCart(int productNumber, String category) {
+
+        ProductsContainer productsContainer = new ProductsContainer(getDriver());
+        productsContainer.setProductThumbnail(category, productNumber);
+        productsContainer.getProductThumbnail().clickAddToCartButton();
+        productsContainer.getProductThumbnail().waitForSeeCartButton();
+    }
+
+    @And("The shopping cart page is open")
+    public void theShoppingCartPagesIsOpen() {
+
+        SiteHeaderCart siteHeaderCart = new SiteHeaderCart(getDriver());
+        siteHeaderCart.clickCartMenu();
+    }
+}
