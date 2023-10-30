43aa8a95576347d1d4e940a29d5a32b9c5c89b53
aksi1984 <pablo_xmen@op.pl>
2023-10-30 10:09:53 +0100

    Created the CreditCardForm class

---
+++ src/main/java/qa/pages/checkout/CreditCardForm.java
@@ -0,0 +1,48 @@
+package qa.pages.checkout;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import qa.base.BasePage;
+
+public class CreditCardForm extends BasePage {
+
+    private final Actions actions;
+    public CreditCardForm(WebDriver driver) {
+
+        super(driver);
+
+        actions = new Actions(driver);
+    }
+
+    @FindBy(id = "stripe-card-element")
+    WebElement cardNumberField;
+
+    @FindBy(id = "stripe-exp-element")
+    WebElement expirationDateField;
+
+    @FindBy(id = "stripe-cvc-element")
+    WebElement cvcField;
+
+    private void set(WebElement element, String data) {
+
+        actions.moveToElement(element).click().perform();
+        actions.sendKeys(data).perform();
+    }
+
+    public void setCardNumber(String cardNumber) {
+
+        set(cardNumberField, cardNumber);
+    }
+
+    public void setExpirationDate(String expirationDate) {
+
+        set(expirationDateField, expirationDate);
+    }
+
+    public void setCVC(String CVC) {
+
+        set(cvcField, CVC);
+    }
+}
