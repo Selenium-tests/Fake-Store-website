7e0f03f432c7ad04fcfbc9551b607a399861fc9f
aksi1984 <pablo_xmen@op.pl>
2023-10-30 23:12:17 +0100

    Created the Runner class

---
+++ src/test/java/com/stepdefs/checkout/expirationdate/Runner.java
@@ -0,0 +1,16 @@
+package com.stepdefs.checkout.expirationdate;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "./src/main/resources/features/checkout/expirationDate.feature",
+        plugin = { "pretty", "html:target/cucumber-reports/creditCardExpirationDate.html" },
+        glue = { "com.stepdefs.checkout.creditcardnumber", "com.commonstepdefs", "com.testutil" },
+        tags = "@ED_DateInThePast"
+)
+public class Runner {
+}
