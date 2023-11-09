d7ba0cb14b1fd7b64e8f6ff9549ce09f6b6e9acb
aksi1984 <pablo_xmen@op.pl>
2023-11-09 21:38:28 +0100

    Created the runner

---
+++ src/test/java/com/stepdefs/checkout/termslink/Runner.java
@@ -0,0 +1,17 @@
+package com.stepdefs.checkout.termslink;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "./src/main/resources/features/checkout/termsLink.feature",
+        plugin = { "pretty", "html:target/cucumber-reports/termsLink.html" },
+        glue = { "com.stepdefs.checkout.termslink",
+                 "com.commonstepdefs",
+                 "com.testutil" }
+)
+public class Runner {
+}
