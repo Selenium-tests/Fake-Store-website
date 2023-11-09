e1083bc2127affb1d2d6a2f46893f90cdd1cc674
aksi1984 <pablo_xmen@op.pl>
2023-11-09 21:39:14 +0100

    Created the TermsLinkStepDefs class

---
+++ src/test/java/com/stepdefs/checkout/termslink/TermsLinkStepDefs.java
@@ -0,0 +1,31 @@
+package com.stepdefs.checkout.termslink;
+
+import com.testutil.TestUtil;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.testng.Assert;
+import qa.pages.checkout.CheckoutPage;
+
+public class TermsLinkStepDefs {
+
+    private final TestUtil testUtil;
+    private final CheckoutPage checkoutPage;
+
+    public TermsLinkStepDefs(TestUtil testUtil) {
+
+        this.testUtil = testUtil;
+        checkoutPage = new CheckoutPage(testUtil.getDriver());
+    }
+
+    @And("Clicks the \"regulamin\" link")
+    public void clicksTheTermsLink() {
+
+        checkoutPage.clickTermsLink();
+    }
+
+    @Then("Information about the regulations has been displayed")
+    public void termsAndConditionsHaveBeenDisplayed() {
+
+        Assert.assertTrue(checkoutPage.isTermsAndConditionsTextBoxVisible());
+    }
+}
