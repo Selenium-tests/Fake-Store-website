950362ef28e9994af8ba8e7dac0316a74e2f2337
aksi1984 <pablo_xmen@op.pl>
2023-11-09 21:43:35 +0100

    Created the CheckoutCommonSteps class

---
+++ src/test/java/com/commonstepdefs/CheckoutCommonSteps.java
@@ -0,0 +1,42 @@
+package com.commonstepdefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+import qa.factories.AddressFormFactory;
+import qa.factories.CreditCardFormFactory;
+import qa.pages.ShoppingCart;
+import qa.pages.checkout.CheckoutPage;
+
+import static qa.driver.Driver.getDriver;
+
+public class CheckoutCommonSteps {
+
+    @When("The user clicks the 'Przejdź do płatności' button")
+    public void theUserClicksTheButton() {
+
+        ShoppingCart shoppingCart = new ShoppingCart(getDriver());
+        shoppingCart.clickCheckoutButton();
+    }
+
+    @And("Fills the credit card form")
+    public void fillsTheCreditCardForm() {
+
+        CreditCardFormFactory.get(getDriver());
+    }
+
+    @And("Fills the payment details form")
+    public void fillsThePaymentDetailsForm() {
+
+        CheckoutPage checkoutPage = new CheckoutPage(getDriver());
+
+        checkoutPage.setAddressForm(AddressFormFactory.get(getDriver()));
+    }
+
+    @And("Accepts terms")
+    public void acceptsTerms() {
+
+        CheckoutPage checkoutPage = new CheckoutPage(getDriver());
+
+        checkoutPage.clickTermsCheckbox();
+    }
+}
