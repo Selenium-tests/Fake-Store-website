f2bb6d4e1cf56ad39e4df776bbe534dae60bc7fc
aksi1984 <pablo_xmen@op.pl>
2023-11-09 20:43:49 +0100

    Created the runner

---
+++ src/test/java/com/stepdefs/checkout/addressform/Runner.java
@@ -0,0 +1,16 @@
+package com.stepdefs.checkout.addressform;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "./src/main/resources/features/checkout/addressForm.feature",
+        plugin = { "pretty", "html:target/cucumber-reports/clickingButton.html" },
+        glue = { "com.stepdefs.checkout.addressform", "com.stepdefs.checkout.creditcardnumber", "com.commonstepdefs", "com.testutil" },
+        tags = "@BTN_withoutEmail"
+)
+public class Runner {
+}
---
+++ src/test/java/com/stepdefs/checkout/checkbox/Runner.java
@@ -0,0 +1,15 @@
+package com.stepdefs.checkout.checkbox;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "./src/main/resources/features/checkout/checkbox.feature",
+        plugin = { "pretty", "html:target/cucumber-reports/checkbox.html" },
+        glue = { "com.stepdefs.checkout.addressform", "com.stepdefs.checkout.checkbox", "com.stepdefs.checkout.creditcardnumber", "com.commonstepdefs", "com.testutil" }
+)
+public class Runner {
+}
