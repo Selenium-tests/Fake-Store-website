83950c9c7ff8b7a17bf7f5d8adcb4a09d8086720
aksi1984 <pablo_xmen@op.pl>
2023-11-09 20:38:13 +0100

    Added static methods

--- src/main/java/qa/factories/AddressFormFactory.java
+++ src/main/java/qa/factories/AddressFormFactory.java
@@ -23,4 +23,130 @@ public class AddressFormFactory {
 
         return addressForm;
     }
+
+    public static AddressForm withoutFirstName(WebDriver driver) {
+
+        AddressForm addressForm = new AddressForm(driver, "billing");
+
+        addressForm.getCountryDropdownList().clickArrow();
+        addressForm.getCountryDropdownList().setCountry("Polska");
+        addressForm.getCountryDropdownList().submit();
+        addressForm.setLastName("Koczuba");
+        addressForm.setCompany("MaxArt");
+        addressForm.setAddress1("ul. Koszarska 5a");
+        addressForm.setPostcode("45-400");
+        addressForm.setCity("Koszalin");
+        addressForm.setPhone("111222333");
+        addressForm.setEmail("annkoczuba@gmail.com");
+
+        return addressForm;
+    }
+
+    public static AddressForm withoutLastName(WebDriver driver) {
+
+        AddressForm addressForm = new AddressForm(driver, "billing");
+
+        addressForm.getCountryDropdownList().clickArrow();
+        addressForm.getCountryDropdownList().setCountry("Polska");
+        addressForm.getCountryDropdownList().submit();
+        addressForm.setFirstName("Anna");
+        addressForm.setCompany("MaxArt");
+        addressForm.setAddress1("ul. Koszarska 5a");
+        addressForm.setPostcode("45-400");
+        addressForm.setCity("Koszalin");
+        addressForm.setPhone("111222333");
+        addressForm.setEmail("annkoczuba@gmail.com");
+
+        return addressForm;
+    }
+
+    public static AddressForm withoutAddress(WebDriver driver) {
+
+        AddressForm addressForm = new AddressForm(driver, "billing");
+
+        addressForm.getCountryDropdownList().clickArrow();
+        addressForm.getCountryDropdownList().setCountry("Polska");
+        addressForm.getCountryDropdownList().submit();
+        addressForm.setFirstName("Anna");
+        addressForm.setLastName("Koczuba");
+        addressForm.setCompany("MaxArt");
+        addressForm.setPostcode("45-400");
+        addressForm.setCity("Koszalin");
+        addressForm.setPhone("111222333");
+        addressForm.setEmail("annkoczuba@gmail.com");
+
+        return addressForm;
+    }
+
+    public static AddressForm withoutPostcode(WebDriver driver) {
+
+        AddressForm addressForm = new AddressForm(driver, "billing");
+
+        addressForm.getCountryDropdownList().clickArrow();
+        addressForm.getCountryDropdownList().setCountry("Polska");
+        addressForm.getCountryDropdownList().submit();
+        addressForm.setFirstName("Anna");
+        addressForm.setLastName("Koczuba");
+        addressForm.setCompany("MaxArt");
+        addressForm.setAddress1("ul. Koszarska 5a");
+        addressForm.setCity("Koszalin");
+        addressForm.setPhone("111222333");
+        addressForm.setEmail("annkoczuba@gmail.com");
+
+        return addressForm;
+    }
+
+    public static AddressForm withoutCity(WebDriver driver) {
+
+        AddressForm addressForm = new AddressForm(driver, "billing");
+
+        addressForm.getCountryDropdownList().clickArrow();
+        addressForm.getCountryDropdownList().setCountry("Polska");
+        addressForm.getCountryDropdownList().submit();
+        addressForm.setFirstName("Anna");
+        addressForm.setLastName("Koczuba");
+        addressForm.setCompany("MaxArt");
+        addressForm.setAddress1("ul. Koszarska 5a");
+        addressForm.setPostcode("45-400");
+        addressForm.setPhone("111222333");
+        addressForm.setEmail("annkoczuba@gmail.com");
+
+        return addressForm;
+    }
+
+    public static AddressForm withoutPhone(WebDriver driver) {
+
+        AddressForm addressForm = new AddressForm(driver, "billing");
+
+        addressForm.getCountryDropdownList().clickArrow();
+        addressForm.getCountryDropdownList().setCountry("Polska");
+        addressForm.getCountryDropdownList().submit();
+        addressForm.setFirstName("Anna");
+        addressForm.setLastName("Koczuba");
+        addressForm.setCompany("MaxArt");
+        addressForm.setAddress1("ul. Koszarska 5a");
+        addressForm.setPostcode("45-400");
+        addressForm.setCity("Koszalin");
+        addressForm.setEmail("annkoczuba@gmail.com");
+
+        return addressForm;
+    }
+
+    public static AddressForm withoutEmail(WebDriver driver) {
+
+        AddressForm addressForm = new AddressForm(driver, "billing");
+
+        addressForm.getCountryDropdownList().clickArrow();
+        addressForm.getCountryDropdownList().setCountry("Polska");
+        addressForm.getCountryDropdownList().submit();
+        addressForm.setFirstName("Anna");
+        addressForm.setLastName("Koczuba");
+        addressForm.setCompany("MaxArt");
+        addressForm.setAddress1("ul. Koszarska 5a");
+        addressForm.setPostcode("45-400");
+        addressForm.setCity("Koszalin");
+        addressForm.setPhone("111222333");
+
+        return addressForm;
+    }
 }
