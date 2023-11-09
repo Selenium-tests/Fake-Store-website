d429fb1c7ada519efa35d465a5da5d618b96dffc
aksi1984 <pablo_xmen@op.pl>
2023-11-09 20:38:55 +0100

    Created the AddressFormStepDefs class

---
+++ src/test/java/com/stepdefs/checkout/addressform/AddressFormStepDefs.java
@@ -0,0 +1,86 @@
+package com.stepdefs.checkout.addressform;
+
+import com.testutil.TestUtil;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.testng.Assert;
+import qa.factories.AddressFormFactory;
+import qa.factories.CreditCardFormFactory;
+import qa.pages.addressform.AddressForm;
+import qa.pages.checkout.CheckoutPage;
+
+import static qa.driver.Driver.getDriver;
+
+public class AddressFormStepDefs {
+
+    private final TestUtil testUtil;
+
+    public AddressFormStepDefs(TestUtil testUtil) {
+
+        this.testUtil = testUtil;
+    }
+
+    @And("Fills the credit card form")
+    public void fillsTheCreditCardForm() {
+
+        CreditCardFormFactory.get(testUtil.getDriver());
+    }
+
+    @And("Fills the address form leaving the 'Imię' field blank")
+    public void fillsTheAddressFormLeavesTheFirstNameFieldBlank() {
+
+        AddressFormFactory.withoutFirstName(testUtil.getDriver());
+    }
+
+    @And("Fills the address form leaving the 'Nazwisko' field blank")
+    public void fillsTheAddressFormLeavesTheLastNameFieldBlank() {
+
+        AddressFormFactory.withoutLastName(testUtil.getDriver());
+    }
+
+    @And("Fills the address form leaving the 'Ulica' field blank")
+    public void fillsTheAddressFormLeavesTheAddressFieldBlank() {
+
+        AddressFormFactory.withoutAddress(testUtil.getDriver());
+    }
+
+    @And("Fills the address form leaving the 'Kod pocztowy' field blank")
+    public void fillsTheAddressFormLeavesThePostcodeFieldBlank() {
+
+        AddressFormFactory.withoutPostcode(testUtil.getDriver());
+    }
+
+    @And("Fills the address form leaving the 'Miasto' field blank")
+    public void fillsTheAddressFormLeavesTheCItyFieldBlank() {
+
+        AddressFormFactory.withoutCity(testUtil.getDriver());
+    }
+
+    @And("Fills the address form leaving the 'Telefon' field blank")
+    public void fillsTheAddressFormLeavesThePhoneFieldBlank() {
+
+        AddressFormFactory.withoutPhone(testUtil.getDriver());
+    }
+
+    @And("Fills the address form leaving the 'Adres email' field blank")
+    public void fillsTheAddressFormLeavesTheEmailFieldBlank() {
+
+        AddressFormFactory.withoutEmail(testUtil.getDriver());
+    }
+
+    @And("Accepts terms")
+    public void acceptsTerms() {
+
+        CheckoutPage checkoutPage = new CheckoutPage(getDriver());
+
+        checkoutPage.clickTermsCheckbox();
+    }
+
+    @Then("A message about not accepting the terms was displayed")
+    public void messageAboutNotAcceptingTermsHasBeenDisplayed() {
+
+        AddressForm addressForm = new AddressForm(testUtil.getDriver(), "billing");
+
+        Assert.assertTrue(addressForm.isErrorMessageDisplayed());
+    }
+}
