f0f9cc67fd17d105bf9bf9ffa48caaca086407a9
aksi1984 <pablo_xmen@op.pl>
2023-11-20 22:25:12 +0100

    Added the Clickable class

--- src/main/java/qa/pages/MainMenu.java
+++ src/main/java/qa/pages/MainMenu.java
@@ -2,22 +2,20 @@ package qa.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import qa.base.BasePage;
+import qa.interactions.clickable.ClickWithClickMethod;
 
 public class MainMenu extends BasePage {
 
     public MainMenu(WebDriver driver) {
 
         super(driver);
+
+        setClickable(new ClickWithClickMethod(driver));
     }
 
-    @FindBy(id = "menu-menu")
-    WebElement navMenu;
+    public void click(String index) throws IllegalAccessException {
 
-    public void click(String index) {
-
-        getDriver().findElement(By.linkText(index)).click();
+        clickable.click(getDriver().findElement(By.linkText(index)));
     }
 }
