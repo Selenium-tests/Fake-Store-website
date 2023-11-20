58e222d332d6b08198de50e52b870d028738b2fc
aksi1984 <pablo_xmen@op.pl>
2023-11-20 07:02:09 +0100

    Removed unnecessary methods

--- src/main/java/qa/base/BasePage.java
+++ src/main/java/qa/base/BasePage.java
@@ -1,26 +1,21 @@
 package qa.base;
 
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
+import qa.interactions.clickable.Clickable;
+import qa.interactions.formfillable.FormFillable;
 
 public class BasePage {
 
     private final WebDriver driver;
-    private final WebDriverWait wait;
-    private final JavascriptExecutor javascriptExecutor;
+    //private final WebDriverWait wait;
+    protected Clickable clickable;
+    protected FormFillable formFillable;
 
     public BasePage(WebDriver driver) {
 
         this.driver = driver;
-
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        javascriptExecutor = (JavascriptExecutor) driver;
 
         PageFactory.initElements(this.driver, this);
     }
@@ -33,28 +28,18 @@ public class BasePage {
         notice.click();
     }
 
-    protected void clickElement(WebElement element) {
-
-        javascriptExecutor.executeScript("arguments[0].click()", element);
-    }
-
-    public String getTitle() {
-
-        return driver.getTitle();
-    }
-
-    public String getUrl() {
-
-        return driver.getCurrentUrl();
-    }
-
     protected WebDriver getDriver() {
 
         return driver;
     }
 
-    protected WebDriverWait getWait() {
+    protected void setClickable(Clickable clickable) {
 
-        return wait;
+        this.clickable = clickable;
+    }
+
+    protected void setFormFillable(FormFillable formFillable) {
+
+        this.formFillable = formFillable;
     }
 }
