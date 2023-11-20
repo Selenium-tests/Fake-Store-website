2d2069e00838b871f96fa090d2bcf5c86d011fa7
aksi1984 <pablo_xmen@op.pl>
2023-11-20 18:04:56 +0100

    Added the Clickable class

--- src/main/java/qa/pages/Addresses.java
+++ src/main/java/qa/pages/Addresses.java
@@ -5,19 +5,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import qa.base.BasePage;
+import qa.interactions.clickable.ClickWithJSExecutor;
 
 public class Addresses extends BasePage {
 
     public Addresses(WebDriver driver) {
 
         super(driver);
+
+        setClickable(new ClickWithJSExecutor(driver));
     }
 
     @FindBy(xpath = ".//div[@class='u-columns woocommerce-Addresses col2-set addresses']")
     WebElement columns;
 
-    public void clickAddButton(int index) {
+    public void clickAddButton(int index) throws IllegalAccessException {
 
-        clickElement(columns.findElements(By.xpath(".//a[@href]")).get(index));
+        clickable.click(columns.findElements(By.xpath(".//a[@href]")).get(index));
     }
 }
