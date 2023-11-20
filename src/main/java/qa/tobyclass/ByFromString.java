0f04b71bc910e88d7540f3cb4cfc28fae0f7dd0a
aksi1984 <pablo_xmen@op.pl>
2023-11-20 07:03:38 +0100

    Added next cases

--- src/main/java/qa/tobyclass/ByFromString.java
+++ src/main/java/qa/tobyclass/ByFromString.java
@@ -13,11 +13,42 @@ public class ByFromString {
             throw new IllegalStateException(String.format("Locator definition does not had 2 elements for %s locator", locatorToString));
         }
 
-        String locatorType = locatorSplit[0];
-        String locatorValue = locatorSplit[1];
+        String using = locatorSplit[0];
+        String value = locatorSplit[1];
 
-        return switch (locatorType) {
-            case "By.cssSelector" -> By.cssSelector(locatorValue);
+        switch (using) {
+            case "By.cssSelector", "css selector" -> {
+                return By.cssSelector(value);
+            }
+            case "By.id", "id" -> {
+                return By.id(value);
+            }
+            case "By.linkText", "link text" -> {
+                return By.linkText(value);
+            }
+            case "By.partialLinkText", "partial link text" -> {
+                return By.partialLinkText(value);
+            }
+            case "By.tagName", "tag name" -> {
+                return By.tagName(value);
+            }
+            case "By.name", "name" -> {
+                return By.name(value);
+            }
+            case "By.className", "class name" -> {
+                return By.className(value);
+            }
+            case "By.xpath", "xpath" -> {
+                return By.xpath(value);
+            }
+            default -> {
+                throw new IllegalStateException("Cannot define locator for WebElement definition " + locatorToString);
+            }
+        }
+
+        /*return switch (locatorType) {
+            case "css selector":
+            case "By.cssSelector": By.cssSelector(locatorValue);
             case "By.id" -> By.id(locatorValue);
             case "By.linkText" -> By.linkText(locatorValue);
             case "By.partialLinkText" -> By.partialLinkText(locatorValue);
@@ -27,6 +58,6 @@ public class ByFromString {
             case "By.xpath" -> By.xpath(locatorValue);
             default ->
                     throw new IllegalStateException("Cannot define locator for WebElement definition " + locatorToString);
-        };
+        };*/
     }
 }
