2720be8cfe845275e3f7346a33f4848d7fb0173d
aksi1984 <pablo_xmen@op.pl>
2023-11-20 07:02:43 +0100

    Added the else if()

--- src/main/java/qa/tobyclass/ByFinder.java
+++ src/main/java/qa/tobyclass/ByFinder.java
@@ -3,15 +3,15 @@ package qa.tobyclass;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
 public class ByFinder {
 
     private static final String BY = "by";
-    private static final String H = "h";
     private static final String LOCATOR = "locator";
-    private static final String UNDERLYING_ELEMENT = "underlyingElement";
     private static final String FOUND_BY = "foundBy";
 
     public static By getByFromWebElement(WebElement element) throws IllegalAccessException {
@@ -19,12 +19,23 @@ public class ByFinder {
         if (element instanceof Proxy proxy) {
 
             InvocationHandler handler = Proxy.getInvocationHandler(proxy);
-            Object field = FieldUtils.readField(handler, LOCATOR, true);
 
+            Object field = FieldUtils.readField(handler, LOCATOR, true);
             Object locator = FieldUtils.readField(field, BY, true);
 
             return ByFromString.getByFromString(locator.toString());
+
+        } else if (element instanceof RemoteWebElement webElement) {
+
+            Object field = FieldUtils.readField(webElement, FOUND_BY, true);
+            String[] split = field.toString().split("-> ");
+
+            return ByFromString.getByFromString(split[1]);
+
+        } else {
+
+            return null;
         }
-        return null;
+
     }
 }
