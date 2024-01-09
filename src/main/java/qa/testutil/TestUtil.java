2a2abd3f52302489eed33112494f4c46fc4afe7f
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-09 18:32:04 +0100

    Added method

--- src/main/java/qa/testutil/TestUtil.java
+++ src/main/java/qa/testutil/TestUtil.java
@@ -12,20 +12,25 @@ public class TestUtil {
     private WebDriver driver;
 
     @Before
-    public void init() {
+    public void setUp() {
 
         driver = WebDriverProvider.getFactory(Browser.CHROME).createDriver();
-        BrowserRunner.run(driver);
-    }
-
-    public WebDriver getDriver() {
-
-        return driver;
+        driver.manage().window().maximize();
     }
 
     @After
     public void tearDown() {
 
         BrowserRunner.quit(driver);
+    }
+
+    public void goToUrl(String url) {
+
+        driver.get(url);
+    }
+
+    public WebDriver getDriver() {
+
+        return driver;
     }
 }
