c4a672404895d88731abd6baf3b840126a4f32b8
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-10 22:12:05 +0100

    Added waiting

--- src/main/java/qa/pages/mainmenu/MainMenu.java
+++ src/main/java/qa/pages/mainmenu/MainMenu.java
@@ -2,20 +2,21 @@ package qa.pages.mainmenu;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import qa.base.BasePage;
-import qa.interactions.clickable.ClickWithClickMethod;
+import qa.enums.PerformType;
 
 public class MainMenu extends BasePage {
 
     public MainMenu(WebDriver driver) {
 
         super(driver);
-
-        setClickable(new ClickWithClickMethod(driver));
     }
 
     public void click(String index) throws IllegalAccessException {
 
-        clickable.click(getDriver().findElement(By.linkText(index)));
+        WebElement link = getWebDriverWait().until(ExpectedConditions.visibilityOf(getDriver().findElement(By.linkText(index))));
+        getInteractions().click(link, PerformType.CLASS_METHOD);
     }
 }
