8ad121c841fc83e94b2eb47723b910520ad40481
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-10 17:47:58 +0100

    Created the Interactions class

---
+++ src/main/java/qa/interactions/Interactions.java
@@ -0,0 +1,43 @@
+package qa.interactions;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import qa.enums.PerformType;
+
+public class Interactions {
+
+    private final JavascriptExecutor javascriptExecutor;
+    private final Actions actions;
+
+    public Interactions(WebDriver driver) {
+
+        javascriptExecutor = (JavascriptExecutor) driver;
+        actions = new Actions(driver);
+    }
+
+    public void click(WebElement element, PerformType type) {
+
+        switch (type) {
+
+            case CLASS_METHOD -> { element.click(); }
+            case JS_EXECUTOR -> { javascriptExecutor.executeScript("arguments[0].click()", element); }
+            case ACTIONS -> { actions.moveToElement(element).click().perform(); }
+        }
+    }
+
+    public void fill(WebElement element, PerformType type, CharSequence... keys) {
+
+        switch (type) {
+
+            case CLASS_METHOD -> { element.sendKeys(keys); }
+            case ACTIONS -> { actions.moveToElement(element).click().perform(); actions.sendKeys(keys); }
+        }
+    }
+
+    public void scrollIntoView(WebElement element) {
+
+        javascriptExecutor.executeScript("arguments[0].scrollIntoView();", element);
+    }
+}
