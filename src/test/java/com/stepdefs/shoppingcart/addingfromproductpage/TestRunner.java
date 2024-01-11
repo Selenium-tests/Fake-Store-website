74e5b76fc7de64305221bbf8153d74e38daf76cf
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-11 17:36:07 +0100

    Moved

--- src/test/java/com/stepdefs/shoppingcart/addingfromproductpage/TestRunner.java
+++ src/test/java/com/stepdefs/shoppingcart/addingfromproductpage/TestRunner.java
@@ -1,4 +1,4 @@
-package com.stepdefs.shoppingcart.addingproduct;
+package com.stepdefs.shoppingcart.addingfromproductpage;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
@@ -7,11 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        features = "./src/main/resources/features/shoppingCart/addingProduct.feature",
+        features = "./src/main/resources/features/shoppingCart/addingFromProductPage.feature",
         plugin = {"pretty", "html:target/cucumber-reports/addingProductToTheCart.html"},
-        glue = { "com.stepdefs.shoppingcart.addingproduct",
+        glue = { "com.stepdefs.shoppingcart.addingfromproductpage",
                  "com.commonstepdefs",
                  "qa.testutil" }
 )
-public class Runner {
+public class TestRunner {
 }
