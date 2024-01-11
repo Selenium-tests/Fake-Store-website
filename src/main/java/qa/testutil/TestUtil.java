0db29a831c2e8787d27ea77b91c1cf0352c6facc
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-11 17:39:02 +0100

    Added logger

--- src/main/java/qa/testutil/TestUtil.java
+++ src/main/java/qa/testutil/TestUtil.java
@@ -3,25 +3,33 @@ package qa.testutil;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
-import qa.browserrunner.BrowserRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import qa.driver.WebDriverProvider;
 import qa.enums.Browser;
+
+import java.time.Duration;
 
 public class TestUtil {
 
     private WebDriver driver;
+    private final Logger logger = LoggerFactory.getLogger(TestUtil.class);
 
     @Before
     public void setUp() {
 
+        logger.info("Initializing WebDriver...");
+
         driver = WebDriverProvider.getFactory(Browser.CHROME).createDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
     }
 
     @After
     public void tearDown() {
 
-        BrowserRunner.quit(driver);
+        logger.info("Quit");
+        driver.quit();
     }
 
     public void goToUrl(String url) {
