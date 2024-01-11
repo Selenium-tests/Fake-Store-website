772e502f3f532983093964883990f43b16e87d73
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-11 17:31:13 +0100

    Created

---
+++ src/main/java/qa/utils/ProductData.java
@@ -0,0 +1,16 @@
+package qa.utils;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+public class ProductData {
+
+    @Setter
+    @Getter
+    private static String name;
+
+    @Setter
+    @Getter
+    private static String price;
+}
