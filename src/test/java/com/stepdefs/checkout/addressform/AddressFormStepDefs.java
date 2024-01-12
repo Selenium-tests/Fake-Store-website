eabebad67370128ad476bcbe9d43c9f35faaea33
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-12 22:40:03 +0100

    Changed annotations

--- src/test/java/com/stepdefs/checkout/addressform/AddressFormStepDefs.java
+++ src/test/java/com/stepdefs/checkout/addressform/AddressFormStepDefs.java
@@ -1,8 +1,8 @@
 package com.stepdefs.checkout.addressform;
 
+import io.cucumber.java.en.When;
 import qa.enums.AddressFormMethods;
 import qa.testutil.TestUtil;
-import io.cucumber.java.en.And;
 import qa.helpers.fillers.AddressFormFiller;
 
 import java.lang.reflect.InvocationTargetException;
@@ -21,43 +21,43 @@ public class AddressFormStepDefs {
         AddressFormFiller.fill(testUtil.getDriver(), omitted);
     }
 
-    @And("Fills the address form leaving the 'Imię' field blank")
+    @When("The user fills the address form leaving the 'Imię' field blank")
     public void fillsTheAddressFormLeavesTheFirstNameFieldBlank() throws IllegalAccessException, InvocationTargetException {
 
         fillAddressForm(AddressFormMethods.SET_FIRST_NAME);
     }
 
-    @And("Fills the address form leaving the 'Nazwisko' field blank")
+    @When("The user fills the address form leaving the 'Nazwisko' field blank")
     public void fillsTheAddressFormLeavesTheLastNameFieldBlank() throws IllegalAccessException, InvocationTargetException {
 
         fillAddressForm(AddressFormMethods.SET_LAST_NAME);
     }
 
-    @And("Fills the address form leaving the 'Ulica' field blank")
+    @When("The user fills the address form leaving the 'Ulica' field blank")
     public void fillsTheAddressFormLeavesTheAddressFieldBlank() throws IllegalAccessException, InvocationTargetException {
 
         fillAddressForm(AddressFormMethods.SET_ADDRESS_1);
     }
 
-    @And("Fills the address form leaving the 'Kod pocztowy' field blank")
+    @When("The user fills the address form leaving the 'Kod pocztowy' field blank")
     public void fillsTheAddressFormLeavesThePostcodeFieldBlank() throws IllegalAccessException, InvocationTargetException {
 
         fillAddressForm(AddressFormMethods.SET_POSTCODE);
     }
 
-    @And("Fills the address form leaving the 'Miasto' field blank")
+    @When("The user fills the address form leaving the 'Miasto' field blank")
     public void fillsTheAddressFormLeavesTheCItyFieldBlank() throws IllegalAccessException, InvocationTargetException {
 
         fillAddressForm(AddressFormMethods.SET_CITY);
     }
 
-    @And("Fills the address form leaving the 'Telefon' field blank")
+    @When("The user fills the address form leaving the 'Telefon' field blank")
     public void fillsTheAddressFormLeavesThePhoneFieldBlank() throws IllegalAccessException, InvocationTargetException {
 
         fillAddressForm(AddressFormMethods.SET_PHONE);
     }
 
-    @And("Fills the address form leaving the 'Adres email' field blank")
+    @When("The user fills the address form leaving the 'Adres email' field blank")
     public void fillsTheAddressFormLeavesTheEmailFieldBlank() throws IllegalAccessException, InvocationTargetException {
 
         fillAddressForm(AddressFormMethods.SET_EMAIL);
