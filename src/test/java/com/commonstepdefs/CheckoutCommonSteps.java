a0fd913946082300f7eb9ca49bd3880261a351c2
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-12 22:30:36 +0100

    Added steps

--- src/test/java/com/commonstepdefs/CheckoutCommonSteps.java
+++ src/test/java/com/commonstepdefs/CheckoutCommonSteps.java
@@ -1,12 +1,9 @@
 package com.commonstepdefs;
 
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.When;
 import qa.enums.AddressFormMethods;
-import qa.enums.CreditCardFormMethods;
 import qa.helpers.fillers.AddressFormFiller;
 import qa.helpers.fillers.CreditCardFormFiller;
-import qa.pages.shoppingcart.ShoppingCart;
 import qa.pages.checkout.CheckoutPage;
 import qa.testutil.TestUtil;
 
@@ -21,23 +18,28 @@ public class CheckoutCommonSteps {
         this.testUtil = testUtil;
     }
 
+    @And("The payment details form is filled")
+    public void paymentDetailsFormIsFilled() throws IllegalAccessException, InvocationTargetException {
+
+        AddressFormFiller.fill(testUtil.getDriver(), AddressFormMethods.ALL);
+    }
+
+    @And("The credit card form is filled")
+    public void creditCardFormIsFilled() {
+
+        CreditCardFormFiller.fill(testUtil.getDriver());
+    }
+
     @And("The checkout page is open")
     public void theCheckoutPageIsOpen() throws IllegalAccessException {
 
-        ShoppingCart shoppingCart = new ShoppingCart(testUtil.getDriver());
-        shoppingCart.clickCheckoutButton();
+        testUtil.goToUrl("https://fakestore.testelka.pl/zamowienie/");
     }
 
     @And("Fills the credit card form")
     public void fillsTheCreditCardForm() {
 
-        CreditCardFormFiller.fill(testUtil.getDriver(), CreditCardFormMethods.ALL);
-    }
-
-    @And("Fills the payment details form")
-    public void fillsThePaymentDetailsForm() throws IllegalAccessException, InvocationTargetException {
-
-        AddressFormFiller.fill(testUtil.getDriver(), AddressFormMethods.ALL);
+        CreditCardFormFiller.fill(testUtil.getDriver());
     }
 
     @And("Accepts terms")
