c627eb20f30965b945e6632bf47b7e2b20e38324
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-13 22:51:10 +0100

    Changed the method body

--- src/main/java/qa/helpers/fillers/CreditCardFormFiller.java
+++ src/main/java/qa/helpers/fillers/CreditCardFormFiller.java
@@ -1,38 +1,16 @@
 package qa.helpers.fillers;
 
 import org.openqa.selenium.WebDriver;
-import qa.enums.CreditCardFormMethods;
 import qa.pages.checkout.CreditCardForm;
-import qa.utils.MethodsRetriever;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.stream.IntStream;
+import qa.utils.CreditCardData;
 
 public class CreditCardFormFiller {
 
-    private static final String[] data ={
-            "332",
-            "4242424242424242",
-            "09/25"
-    };
+    private static final CreditCardData creditCardData = new CreditCardData("4242424242424242", "09/25", "332");
 
-    public static CreditCardForm fill(WebDriver driver, CreditCardFormMethods omitted) {
+    public static void fill(WebDriver driver) {
 
-        MethodsRetriever<CreditCardForm> retriever = new MethodsRetriever<>(CreditCardForm.class);
-        Method[] methods = retriever.getMethods();
         CreditCardForm creditCardForm = new CreditCardForm(driver);
-
-        IntStream.range(0, methods.length)
-                .filter(i -> !methods[i].getName().toLowerCase().contains(omitted.getName().toLowerCase()))
-                .forEach(i -> {
-                    try {
-                        methods[i].invoke(creditCardForm, data[i]);
-                    } catch (IllegalAccessException | InvocationTargetException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-
-        return creditCardForm;
+        creditCardForm.setCreditCardData(creditCardData);
     }
 }
