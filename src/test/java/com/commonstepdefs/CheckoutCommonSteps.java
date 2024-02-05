aa05966dd92d3a4c3c6b8a2611c4b2b862ac2476
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-05 07:58:42 +0100

    Added the step

--- src/test/java/com/commonstepdefs/CheckoutCommonSteps.java
+++ src/test/java/com/commonstepdefs/CheckoutCommonSteps.java
@@ -1,6 +1,9 @@
 package com.commonstepdefs;
 
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
 import qa.enums.AddressFormMethods;
 import qa.helpers.fillers.AddressFormFiller;
 import qa.helpers.fillers.CreditCardFormFiller;
@@ -17,6 +20,34 @@ public class CheckoutCommonSteps {
 
         this.testUtil = testUtil;
     }
+
+    @When("The user clicks the \"Kupuję i płacę\" button")
+    @And("Clicks the \"Kupuję i płacę\" button")
+    public void clicksTheSubmitButton() throws InterruptedException, IllegalAccessException {
+
+        CheckoutPage checkoutPage = new CheckoutPage(testUtil.getDriver());
+        checkoutPage.clickSubmitButton();
+
+        Thread.sleep(6000);
+    }
+
+    @Then("The message about blank 'Imię' field on the checkout page has been displayed")
+    @Then("The message about blank 'Nazwisko' field on the checkout page has been displayed")
+    @Then("The message about blank 'Ulica' field on the checkout page has been displayed")
+    @Then("The message about blank 'Kod pocztowy' field on the checkout page has been displayed")
+    @Then("The message about blank 'Miasto' field on the checkout page has been displayed")
+    @Then("The message about blank 'Telefon' field on the checkout page has been displayed")
+    @Then("The message about blank 'Adres email' field on the checkout page has been displayed")
+    public void messageOnTheCheckoutPageHasBeenDisplayed() {
+
+        try {
+            CheckoutPage checkoutPage = new CheckoutPage(testUtil.getDriver());
+            checkoutPage.waitForErrorMessageLocator();
+        } catch (IllegalAccessException e) {
+            Assert.fail("The message is not displayed");
+        }
+    }
+
 
     @And("The payment details form is filled")
     public void paymentDetailsFormIsFilled() throws IllegalAccessException, InvocationTargetException {
@@ -43,9 +74,16 @@ public class CheckoutCommonSteps {
     }
 
     @And("Accepts terms")
-    public void acceptsTerms() throws IllegalAccessException {
+    public void acceptsTerms() {
 
         CheckoutPage checkoutPage = new CheckoutPage(testUtil.getDriver());
         checkoutPage.clickTermsCheckbox();
+    }
+
+    @And("The error message on the checkout page is {string}")
+    public void errorMessageOnTheCheckoutPageIs(String message) {
+
+        CheckoutPage checkoutPage = new CheckoutPage(testUtil.getDriver());
+        Assert.assertEquals(checkoutPage.getErrorMessageText(), message);
     }
 }
