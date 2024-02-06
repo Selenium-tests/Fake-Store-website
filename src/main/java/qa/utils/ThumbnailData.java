9c912e6ed811d6b6d234a42e90d1cedc90f90292
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-06 08:11:39 +0100

    Added the argument

--- src/main/java/qa/utils/ThumbnailData.java
+++ src/main/java/qa/utils/ThumbnailData.java
@@ -7,10 +7,12 @@ public class ThumbnailData {
 
     private final String category;
     private final int number;
+    private final String productPageUrl;
 
-    public ThumbnailData(String category, int number) {
+    public ThumbnailData(String category, int number, String urlPart) {
 
         this.category = category;
         this.number = number;
+        this.productPageUrl = "https://fakestore.testelka.pl/product/" + urlPart;
     }
 }
