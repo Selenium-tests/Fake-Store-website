f8025653e8777a911e5410ac649977f005e0a3f0
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-06 08:12:16 +0100

    Updated

--- src/main/java/qa/utils/ThumbnailDataProvider.java
+++ src/main/java/qa/utils/ThumbnailDataProvider.java
@@ -6,8 +6,9 @@ public class ThumbnailDataProvider {
 
     private static final HashMap<String, ThumbnailData> map = new HashMap<>() { {
 
-        put("Windsurfing w Lanzarote (Costa Teguise)", new ThumbnailData("Popularne", 0));
-        put("Yoga i pilates w Karaibach", new ThumbnailData("W promocji", 1));
+        put("Windsurfing w Lanzarote (Costa Teguise)", new ThumbnailData("Popularne", 0, "windsurfing-w-lanzarote-costa-teguise/"));
+        put("Yoga i pilates w Portugalii", new ThumbnailData("W promocji", 1, "yoga-i-pilates-w-portugalii/"));
+        put("Wakacje z yogą w Kraju Kwitnącej Wiśni", new ThumbnailData("W promocji", 0, "wakacje-z-yoga-w-kraju-kwitnacej-wisni/"));
     }};
 
     public static ThumbnailData getThumbnailData(String name) {
