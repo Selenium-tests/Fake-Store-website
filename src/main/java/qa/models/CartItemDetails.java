abe9be1ec2a49c4e8554a4a670b9eb48f02b0ad5
pawelaksman <pablo_xmen@op.pl>
2024-02-07 14:28:23 +0100

    Added the @Getter annotation

--- src/main/java/qa/models/CartItemDetails.java
+++ src/main/java/qa/models/CartItemDetails.java
@@ -3,20 +3,13 @@ package qa.models;
 import lombok.Getter;
 import qa.support.PriceParser;
 
+@Getter
 public class CartItemDetails {
 
-    @Getter
-    private String quantity;
     private final double amount;
 
     public CartItemDetails(String quantity, String price) {
 
-        this.quantity = quantity;
         this.amount = PriceParser.parse(price) * Double.parseDouble(quantity);
-    }
-
-    public double getAmount() {
-
-        return amount;
     }
 }
