5bef56043512329dafd1e2de6d14c28fcdeb2cc5
pawelaksman <pablo_xmen@op.pl>
2024-02-07 14:31:38 +0100

    Updated

--- src/main/java/qa/models/CartItemDetails.java
+++ src/main/java/qa/models/CartItemDetails.java
@@ -6,10 +6,12 @@ import qa.support.PriceParser;
 @Getter
 public class CartItemDetails {
 
+    private final String quantity;
     private final double amount;
 
     public CartItemDetails(String quantity, String price) {
 
+        this.quantity = quantity;
         this.amount = PriceParser.parse(price) * Double.parseDouble(quantity);
     }
 }
