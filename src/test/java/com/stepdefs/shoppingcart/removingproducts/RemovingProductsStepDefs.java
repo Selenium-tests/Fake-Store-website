8ea091ef9fa8f00dbd1c3325f9c60b5bdb8b9750
pawelaksman <pablo_xmen@op.pl>
2024-02-07 14:28:59 +0100

    Updated

--- src/test/java/com/stepdefs/shoppingcart/removingproducts/RemovingProductsStepDefs.java
+++ src/test/java/com/stepdefs/shoppingcart/removingproducts/RemovingProductsStepDefs.java
@@ -1,7 +1,7 @@
 package com.stepdefs.shoppingcart.removingproducts;
 
 import io.cucumber.java.en.When;
-import qa.animation.CouponCodeRefreshLoader;
+import qa.pages.animation.Animation;
 import qa.pages.shoppingcart.ShoppingCart;
 import qa.testutil.TestUtil;
 
@@ -9,24 +9,25 @@ public class RemovingProductsStepDefs {
 
     private final TestUtil testUtil;
     private final ShoppingCart shoppingCart;
-    private final CouponCodeRefreshLoader couponCodeRefreshLoader;
+    private final Animation animation;
 
     public RemovingProductsStepDefs(TestUtil testUtil) {
 
         this.testUtil = testUtil;
         shoppingCart = new ShoppingCart(testUtil.getDriver());
-        couponCodeRefreshLoader = new CouponCodeRefreshLoader(testUtil.getDriver());
+        animation = new Animation(testUtil.getDriver());
     }
 
     @When("The user clicks the 'Remove' button")
-    public void theUserClicksTheRemoveButton() {
+    public void theUserClicksTheRemoveButton() throws IllegalAccessException {
 
         shoppingCart.getTable().findRows();
         shoppingCart.getTable().clickRemoveButton(0);
+        animation.waitUntilIsInvisible();
     }
 
     @When("The user enters the zero value in the quantity field")
-    public void userEnterZeroValue() throws IllegalAccessException {
+    public void userEnterZeroValue() {
 
         shoppingCart.getTable().findRows();
         shoppingCart.getTable().getQuantityField(0).setQuantity("0");
