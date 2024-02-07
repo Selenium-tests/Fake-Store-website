93bafc380982b3e11b7b0ed35ac75ce1edaa4806
pawelaksman <pablo_xmen@op.pl>
2024-02-07 13:00:34 +0100

    Updated

--- src/test/java/com/stepdefs/shoppingcart/removingproducts/TestRunner.java
+++ src/test/java/com/stepdefs/shoppingcart/removingproducts/TestRunner.java
@@ -1,4 +1,4 @@
-package com.stepdefs.shoppingcart.functionalities;
+package com.stepdefs.shoppingcart.removingproducts;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
@@ -6,11 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "./src/main/resources/features/shoppingCart/removeButton.feature",
+        features = "./src/main/resources/features/shoppingCart/removingProducts.feature",
         plugin = { "pretty", "html:target/cucumber-reports/removeButton.html" },
-        glue = {"com.stepdefs.shoppingcart.functionalities",
+        glue = {"com.stepdefs.shoppingcart.removingproducts",
                 "com.commonstepdefs",
-                "qa.testutil" }
+                "qa.testutil" },
+        tags = "@SCCRemovingWithQuantityField"
 )
 public class TestRunner {
 }
