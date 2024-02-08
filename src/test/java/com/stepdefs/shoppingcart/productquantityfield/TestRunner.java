5d40199230d5dc68144ef10c82244d8f719567ee
pawelaksman <pablo_xmen@op.pl>
2024-02-08 21:54:24 +0100

    Updated

--- src/test/java/com/stepdefs/shoppingcart/productquantityfield/TestRunner.java
+++ src/test/java/com/stepdefs/shoppingcart/productquantityfield/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "./src/main/resources/features/shoppingCart/productQuantityField.feature",
+        features = "./src/main/resources/features/shoppingCart/quantityField.feature",
         plugin = { "pretty", "html:target/cucumber-reports/productQuantityField.html" },
         glue = { "com.stepdefs.shoppingcart.productquantityfield",
                  "com.commonstepdefs",
