a3cd6d09327e9d3a4096cdea5df6d14f05e8e9d1
pawelaksman <pablo_xmen@op.pl>
2024-02-08 21:51:47 +0100

    Added steps

--- src/test/java/com/stepdefs/shoppingcart/productquantityfield/ProductQuantityFieldStepDefs.java
+++ src/test/java/com/stepdefs/shoppingcart/productquantityfield/ProductQuantityFieldStepDefs.java
@@ -1,6 +1,7 @@
 package com.stepdefs.shoppingcart.productquantityfield;
 
 import qa.models.CartItemDetails;
+import qa.support.StringCalc;
 import qa.testutil.TestUtil;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
@@ -22,19 +23,60 @@ public class ProductQuantityFieldStepDefs {
         shoppingCart = new ShoppingCart(testUtil.getDriver());
     }
 
-    @When("The user types the {string} as quantity of the product")
-    public void theUserTypesTheQuantityOfTheProduct(String quantity) {
+    private void setValue(String quantity, String rhs, String operator) {
 
-        shoppingCart.getTable().findRows();
-        shoppingCart.getTable().getQuantityField(0).setQuantity(quantity);
-        cartItemDetails = new CartItemDetails(quantity, shoppingCart.getTable().getProductPrice(0));
+        String result = StringCalc.calculate(quantity, rhs, operator);
+        shoppingCart.getTable().getQuantityField(0).setQuantity(result);
+        cartItemDetails = new CartItemDetails(result, shoppingCart.getTable().getProductPrice(0));
     }
 
-    @When("The user types the {string} as not a number")
-    public void theUserTypesNotANumber(String quantity) {
+    @When("The user types quantity: {string}")
+    public void userTypesQuantity(String quantity) {
 
         shoppingCart.getTable().findRows();
         shoppingCart.getTable().getQuantityField(0).setQuantity(quantity);
+    }
+
+    @When("The user types quantity which is MIN - 1")
+    public void userTypesQuantityWhichIsLessThanMin() {
+
+        shoppingCart.getTable().findRows();
+        setValue(shoppingCart.getTable().getQuantityField(0).getMin(), "0", "+");
+    }
+
+    @When("The user types quantity which is MIN")
+    public void userTypesQuantityWhichIsMin() {
+
+        shoppingCart.getTable().findRows();
+        setValue(shoppingCart.getTable().getQuantityField(0).getMin(), "1", "+");
+    }
+
+    @When("The user types quantity which is MIN + 1")
+    public void userTypesQuantityWhichIsGreaterThanMin() {
+
+        shoppingCart.getTable().findRows();
+        setValue(shoppingCart.getTable().getQuantityField(0).getMin(), "2", "+");
+    }
+
+    @When("The user types quantity which is MAX - 1")
+    public void userTypesQuantityWhichLessThanMax() {
+
+        shoppingCart.getTable().findRows();
+        setValue(shoppingCart.getTable().getQuantityField(0).getMax(), "1", "-");
+    }
+
+    @When("The user types quantity which is MAX")
+    public void userTypesQuantityWhichIsMax() {
+
+        shoppingCart.getTable().findRows();
+        setValue(shoppingCart.getTable().getQuantityField(0).getMax(), "0", "+");
+    }
+
+    @When("The user types quantity which is MAX + 1")
+    public void userTypesQuantityWhichGreaterThanMax() {
+
+        shoppingCart.getTable().findRows();
+        setValue(shoppingCart.getTable().getQuantityField(0).getMax(), "1", "+");
     }
 
     @Then("The amount of the product is correct")
@@ -62,7 +104,5 @@ public class ProductQuantityFieldStepDefs {
         } catch (Exception e) {
             Assert.fail("The validation message is not displayed");
         }
-
-        System.out.println(shoppingCart.getTable().getQuantityField(0).getValidationMessageText());
     }
 }
